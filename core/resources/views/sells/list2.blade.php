63fb93d8b4d3961793b23065b55cf764f11d94f6
Esteve Seguí <esteveseguicasas.esc@hotmail.com>
2024-08-12 22:51:09 +0200

    cambios sells

--- core/resources/views/sells/list2.blade.php
+++ core/resources/views/sells/list2.blade.php
@@ -51,17 +51,12 @@
         }
 
         .row {
-            margin-right: 0 !important;
-            margin-left: 0 !important;
+            margin-right: 0px !important;
+            margin-left: 0px !important;
         }
 
         .view-height {
             margin-bottom: 30rem;
-        }
-
-        main {
-            padding: 0;
-            margin: 0;
         }
 
     </style>
@@ -70,21 +65,21 @@
 @section('content')
 
     <div class="container-fluid view-height">
-        <h3 style="text-align: center">Sells</h3>
+        <h3 class="text-center">Sells</h3>
         <div class="row">
             <main role="main" class="col-md-12 px-4">
                 <div class="d-flex justify-content-center">
                     <form action="{{ route('sells.search') }}" method="get">
                         <label for="search">Búsqueda</label>
                         <input type="text" name="search" value="{{ $search ?? '' }}" class="input-lg" />
-                        <button type="submit" class="ml-4 btn btn btn-danger">Buscar</button>
-                        <a href="{{ route('sells.list') }}" class="ml-4 btn btn btn-info">Limpiar</a>
+                        <button type="submit" class="ml-4 btn btn-danger">Buscar</button>
+                        <a href="{{ route('sells.list') }}" class="ml-4 btn btn-info">Limpiar</a>
                     </form>
                 </div>
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     @if ($errors->any())
                         <div class="alert alert-danger">
-                            <h6>Por favor corrige los siguiente errores:</h6>
+                            <h6>Por favor corrige los siguientes errores:</h6>
                             <ul>
                                 @foreach($errors->all() as $error)
                                     <li>{{ $error }}</li>
@@ -92,17 +87,9 @@
                             </ul>
                         </div>
                     @endif
-                    @if(session()->has('result'))
-                        <script type="text/javascript">
-                            Swal.fire({
-                                title: '{{ session()->get('result')->title }}',
-                                text: '{{ session()->get('result')->message }}',
-                                type: '{{ session()->get('result')->type }}',
-                                showCancelButton: false,
-                                confirmButtonColor: '#C6682A'
-                            });
-                        </script>
-                    @endif
+
+                    <button id="añadirCliente" class="btn btn-primary">Añadir venta</button>
+
                     <form action="{{ route('sells.add') }}" method="POST" id="formulario" style="display: none">
                         @csrf
 
@@ -120,16 +107,14 @@
                             @endforeach
                         </select>
 
-
                         <h2>Líneas de venta</h2>
-                        <br/>
                         <div id="sell_lines">
-                            <div id="lines_div">
+                            <div class="line-item">
                                 <label for="quantity">Cantidad</label>
-                                <input  type="number" name="quantity[]" id="quantity" />
+                                <input type="number" name="quantity[]" />
                                 <br/>
                                 <label for="vehicle_id">Vehículo</label>
-                                <select name="vehicle_id[]" id="vehicle_id" class="form-control">
+                                <select name="vehicle_id[]" class="form-control">
                                     @foreach($vehicles as $vehicle)
                                         <option value="{{ $vehicle->id }}">{{ $vehicle->registration }} {{ $vehicle->brand }}</option>
                                     @endforeach
@@ -137,18 +122,14 @@
                             </div>
                         </div>
 
-                        <a href="#" id="add_line" class="btn btn-xs btn-warning">Añadir línea</a>
-
+                        <a href="#" id="add_line" class="btn btn-warning">Añadir línea</a>
                         <button type="submit" class="btn btn-danger">Guardar</button>
                     </form>
-
-                    <button id="añadirCliente" class="ml-4 btn btn-primary">Añadir venta</button>
                 </div>
             </main>
 
             <main role="main" class="col-md-12 px-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    {{--                    <a href="{{ route('main') }}">Volver al menú</a>--}}
                     <table>
                         <tr>
                             <th>ID</th>
@@ -168,40 +149,40 @@
                                 <td>{{ number_format($sell->total(), 2) }}</td>
                                 <td>{{ \Illuminate\Support\Carbon::parse($sell->created_at)->format('d-m-Y H:i:s') }}</td>
                                 <td>
-                                    <a href="{{ route('sells.get', $sell) }}" class="ml-4 btn btn-primary">Ver detalle</a>
+                                    <a href="{{ route('sells.get', $sell) }}" class="btn btn-primary">Ver detalle</a>
                                 </td>
                                 <td>
                                     <form action="{{ route('sells.delete', $sell) }}" method="post">
-                                        {{ csrf_field() }}
-                                        {{ method_field('delete') }}
-                                        <button type="submit" class="ml-4 btn btn btn-danger">Eliminar</button>
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger">Eliminar</button>
                                     </form>
                                 </td>
                             </tr>
                         @endforeach
                     </table>
                 </div>
-                <div class="d-flex justify-content-center">
-                    {{ $sells->links('pagination.custom') }}
-                </div>
+                {{ $sells->links('pagination.custom') }}
             </main>
+        </div>
+        <div class="text-center mt-4">
+            <a href="{{ url('/') }}" class="btn btn-primary">Ir a la página de inicio</a>
         </div>
     </div>
 
-    <div class="text-center mt-4">
-        <a href="{{ url('/') }}" class="ml-4 btn btn-primary">Ir a la página de inicio</a>
-    </div>
+
+
 
 @endsection
 
 @section('js')
     <script href="">
         $(document).ready(function () {
-            $("#add_line").click(function (e) {
-                e.preventDefault();
-                let cloned = $("#lines_div").clone();
-                $("#sell_lines").append(cloned);
-            });
+           $("#add_line").click(function (e) {
+               e.preventDefault();
+               let cloned = $("#lines_div").clone();
+               $("#sell_lines").append(cloned);
+           });
         });
     </script>
     <script>
